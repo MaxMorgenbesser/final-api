16e753b51ce72bc4194b70bb23752c1afff38003
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-09-07 19:03:09 -0400

    has firebase auth now

---
+++ functions/firebaseauth.js
@@ -0,0 +1,12 @@
+export const authcreds= {
+  "type": "service_account",
+  "project_id": "final-mm-fe",
+  "private_key_id": "fa214605b112b0f4dbd89cad56ab8153808354b9",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChzIj0oAz/dncB\nLNEBl0htlbB4sjglQtR//65+oDixvkl0Chbdbq5gIQ1CRx72GIEQoWYcTMUcYZAT\nH7m+PAquugIBSoLLF9t2cxoKLrWfPg1RUSJFasqxZhp9OoslmiMMB8D5uYpArUPc\nEGwk0JBBKbOIohVlbynogV3peOZnaRVrxspY0HrT+4GLR66S41IjRjPiR8WkQzxH\n3+64opT2NaoijTjVi7cK7mS0+85eq2lGThOUc4WtBVQwGJUEdyHydHjECmZ7u5/Y\nDkRzS9RcIf64HeWGYMm86OLiKUugwor3oQtwm9s1uuHY/OXSEfP9GykdAw67ARuc\nokGZSRMxAgMBAAECggEAGdYQVIeuK6jeDej7IvbmUOFjQZKr1n9acWoBkiTBiNhq\n4EWwKwG+hIkild/HeOKm0WIEbSZxHzwRBoy9Wzw5NHdtVIlyYz3XBI4SrWeSBvCg\nrch34X/RCAzW1AJiL8nyKLygOwA+rF8+g8h3jFVlg5JGId5H7l+W4JKvzVVJ7V8g\nJqJ04nI59BWyy64pjGCOtsIYzST9JqY3VVrtCiknDk06HdS01uKJtvYxsj5aPi3C\n+Aw3x503wVmjMRf3x1AMtFCvr/a70FaU6mMkbgAFDAvD3dmsKCeieSzxON/nK446\nwf1+yu/Qbvu42VJWc5V/61cy5KD7+EUcZVgNHzxX5QKBgQDbfsbi/4XfExelkDdR\nlmD+e0Utixv9eE/YcrltpAtNyZ4JAR0qdnTUXoiVCDlcJiVE8XUkQNmZg3ix2ka9\nNf5wvGfvZA/UkiGzdxkuM+PiLT5O8RvE0NO5WJ/XfLgz1OdmkqbbpFc//LwCXmFs\n0pq6AxISs1SjbwlVYIPpUN2TrwKBgQC8tUieNVQwHGNo4Bt2rulWC74CIIblwXTe\n0Ol8L5F/5D/yE1oGA+iQMrk2VU4Q8eaRyAMjC10TQ5Fd2Mm5fZpHYKDtEXGiZgIB\nS4CZ2jfaAzp7UJJ7E+6z8964hZUZxD0IjP3ix4gppfKfEXBAwrO7/672RfgV5Fe/\nt3jR+g4fHwKBgFD1sQxXbZx7YVR7HjxkuEAFXd3PmrJiZykSH90WDelSPY1asNRR\nvwP6HmzCDM41b7e/TBLkLJ7YBHOtbDwsJz6T78433TLt96pHeq01DJAHWh0qB7EP\nyZhq3QJRiWvs0rFd1FfdhSUf5oUFJPBLujB0I31sUgK6WPn50I4FesUXAoGBAJSq\nwUYav3OTueGSxTAwRpXKdGwakgwJ5/fuYDO9CgTWnmYpRr/8bkjEHVQnynPhD4QE\nLix6tF4L2kc0FmBzCot3HfEG0B5f/O5mPC95Uo0IBBfId05Ner38v5dhbgC5sFe/\nM/lotuYQ3d7hjy0F9zGCEqXko7kjhtn8QkwyktODAoGAMTnuhivQf/SjjWpviD3w\nXoUEP8xUIIJ6Rms0fkbdMRV4m8v4YyX0oSAwJL+ye0DWnJD1kRLHIRhHWfnivwqX\nOJG5yFrfCCfvBGaLVN+GZudp9KsV465i1RNiYu2Yt0JAKvGcpKeFdS1krwDo4Z9w\nnUCywiCNhm0i9Em6PlFhsKw=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-29sfq@final-mm-fe.iam.gserviceaccount.com",
+  "client_id": "103125190660266344567",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-29sfq%40final-mm-fe.iam.gserviceaccount.com"
+}
--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,90 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
+
 import { credentials } from "./src/credentials.js";
 import { MongoClient, ObjectId } from "mongodb";
 import pdf from "./pdf-parse/index.js";
+import { authcreds } from "./firebaseauth.js";
+import { getAuth } from "firebase-admin/auth";
+// import {verifyIdToken} from 'firebase-admin/auth'
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+// import {getFirestore } from "firebase-admin/firestore"
 
+export function authConnect() {
+  if (!getApps().length) {
+    initializeApp({
+      credential: cert(authcreds),
+    });
+  }
+  return getAuth();
+}
+
+authConnect();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//connect to firebase
 
+// connects to mongo
 const client = new MongoClient(credentials);
 const db = client.db("final");
 const collection = db.collection("resumes");
 
+const getresumes = async (req, res) => {
+  const token = req.headers.authorization;
+  const auth = await authConnect();
+  auth.verifyIdToken(token).then((decodedToken) => {
+    const uid = decodedToken.uid;
+    console.log(uid);
+    if (uid) {
+      collection.find({}).toArray((err, resumes) => {
+        res.send(resumes);
+      });
+    }
+  });
+};
 
-app.get("/getresumes", (req, res) => {
-  collection.find({}).toArray((err, resumes) => {
-    res.send(resumes);
+app.get("/getresumes", getresumes);
+
+app.post("/addresume", async (req, res) => {
+  const token = req.headers.authorization;
+  const auth = await authConnect();
+  auth.verifyIdToken(token).then((decodedToken) => {
+    const uid = decodedToken.uid;
+    console.log(uid);
+    if (uid) {
+      let newRes = req.body;
+      let filebase = req.body.filebase64;
+      let Fbase64 = filebase.replace("data:application/pdf;base64,", "");
+      let buf = new Buffer.from(Fbase64, "base64");
+      pdf(buf)
+        .then(function (data) {
+          let text = data.text;
+          collection.insertOne({ newRes, words: text });
+          res.send({ text });
+        })
+        .catch((err) => console.log(err));
+    }
   });
 });
 
-
-app.post("/addresume", async (req, res) => {
-  let newRes = req.body;
-  let filebase = req.body.filebase64;
-  let Fbase64 = filebase.replace("data:application/pdf;base64,", "");
-  let buf = new Buffer.from(Fbase64, "base64");
-  pdf(buf)
-    .then(function (data) {
-      let text = data.text;
-      collection.insertOne({ newRes, words: text });
-      res.send({ text });
-    })
-    .catch((err) => console.log(err));
-});
-
-
-
 app.put("/:id", async (req, res) => {
-  await collection.findOneAndUpdate(
-    { _id: new ObjectId(req.params.id) },
-    { $inc: req.body }
-  );
-  res.send({ updated: true });
+  const token = req.headers.authorization;
+  const auth = await authConnect();
+  auth.verifyIdToken(token).then(async (decodedToken) => {
+    const uid = decodedToken.uid;
+    console.log(uid);
+    if (uid) {
+      await collection.findOneAndUpdate(
+        { _id: new ObjectId(req.params.id) },
+        { $inc: req.body }
+      );
+      res.send({ updated: true });
+    }
+  });
 });
 
 // app.put("/userinfo/:id", async (req, res) => {
